2a80f71c4dd3b9b641979dd8f99f463c67514d87
AbiyaY <abiyay30@gmail.com>
2023-09-04 09:48:25 +0100

    Updated Controller
    Updated Controller
    
    Co-Authored-By: asawkhan <119862954+asawkhan@users.noreply.github.com>
    Co-Authored-By: Daniel Onu <57682531+thunder86440@users.noreply.github.com>

--- qeproject/src/main/java/com/example/qeproject/controller/IntergersApiController.java
+++ qeproject/src/main/java/com/example/qeproject/controller/IntergersApiController.java
@@ -4,6 +4,7 @@ import com.example.qeproject.model.Integers;
 import com.example.qeproject.service.IntegersService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpMethod;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
 
@@ -23,13 +24,18 @@ public class IntergersApiController {
         return integersService.getIntegers();
     }
     @PostMapping
-    public Integer enterTwoNumbers(@RequestBody Integers integers){
+    public void enterTwoNumbers(@RequestBody Integers integers){
         integersService.storeIntegers(integers);
         RestTemplate restTemplate = new RestTemplate();
         String resourceUrl = "http://localhost:8080/mathservice/integers";
-        HttpEntity<Integers> request = new HttpEntity<Integers>(integersService.getIntegers());
-        Integer integerCreateResponse = restTemplate
-                .postForObject(resourceUrl, request, Integer.class);
-        return integerCreateResponse;
+//        HttpEntity<Integers> request = new HttpEntity<Integers>(integersService.getIntegers());
+//        restTemplate.exchange(
+//                resourceUrl,
+//                HttpMethod.PUT,
+//                request,
+//                Void.class);
+        Void integerCreateResponse = restTemplate
+                .postForObject(resourceUrl, integersService.getIntegers(), Void.class);
+        //return integerCreateResponse;
     }
 }
--- qeproject/src/main/java/com/example/qeproject/controller/OperatorApiController.java
+++ qeproject/src/main/java/com/example/qeproject/controller/OperatorApiController.java
@@ -1,10 +1,13 @@
 package com.example.qeproject.controller;
 
+import com.example.qeproject.model.Integers;
 import com.example.qeproject.model.Operator;
 import com.example.qeproject.service.IntegersService;
 import com.example.qeproject.service.OperatorService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.RestTemplate;
 
 @RestController
 @RequestMapping("/operatorservice")
@@ -22,9 +25,16 @@ public class OperatorApiController {
     }
 
     @PostMapping
-    public String enterOperator(@RequestBody Operator operator){
+    public Integer enterOperator(@RequestBody Operator operator){
         operatorService.storeOperator(operator);
-        return "Operator entered successfully";
+        RestTemplate restTemplate = new RestTemplate();
+        String resourceUrl = "http://localhost:8080/mathservice/operator";
+        //HttpEntity<Operator> request = new HttpEntity<Operator>(operatorService.getOperator());
+        Integer response = restTemplate
+                .postForObject(resourceUrl, operatorService.getOperator(), Integer.class);
+        return response;
+//        operatorService.storeOperator(operator);
+//        return "Operator entered successfully";
     }
 
 }
