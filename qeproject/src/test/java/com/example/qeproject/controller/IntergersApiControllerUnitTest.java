bba5d4007e33afa7fbd79a2d49a6d9eac3018799
asawkhan <asawerkhan01@gmail.com>
2023-09-06 14:00:10 +0100

    AK: Created a positive and negative unit test for integersApiController

--- qeproject/src/test/java/com/example/qeproject/controller/IntergersApiControllerUnitTest.java
+++ qeproject/src/test/java/com/example/qeproject/controller/IntergersApiControllerUnitTest.java
@@ -1,10 +1,17 @@
 package com.example.qeproject.controller;
 
+import com.example.qeproject.model.Integers;
 import com.example.qeproject.service.IntegersService;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
 @WebMvcTest(IntergersApiController.class)
 public class IntergersApiControllerUnitTest {
@@ -12,8 +19,36 @@ public class IntergersApiControllerUnitTest {
     @Autowired
     MockMvc mockMvc;
 
+    @Autowired
+    ObjectMapper mapper;
+
     @MockBean
     IntegersService service;
 
+    @Test
+    public void postTwoNumbersAsAnObject_isCreated() throws Exception {
 
+        Integers integers = new Integers(2, 5);
+        String json = mapper.writeValueAsString(integers);
+
+        mockMvc.perform(MockMvcRequestBuilders.post("/numberservice")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(json)
+                .accept(MediaType.APPLICATION_JSON))
+                .andExpect(MockMvcResultMatchers.status().isCreated())
+                .andDo(MockMvcResultHandlers.print());
+    }
+
+    @Test
+    public void postStringAsInput_BadRequest() throws Exception {
+
+        String wrongInput = "This is the wrong input type";
+
+        mockMvc.perform(MockMvcRequestBuilders.post("/numberservice")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(wrongInput)
+                        .accept(MediaType.APPLICATION_JSON))
+                .andExpect(MockMvcResultMatchers.status().isBadRequest())
+                .andDo(MockMvcResultHandlers.print());
+    }
 }
